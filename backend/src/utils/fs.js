897727295b654081c5f62cd7af9949a438de2136
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-21 11:49:50 -0700

    Update fs.js

--- backend/src/utils/fs.js
+++ backend/src/utils/fs.js
@@ -1,14 +1,22 @@
 import fs from "fs";
+import path from "path";
 
 export function read(filePath = "") {
-  return fs.readFileSync(filePath, "utf8");
+  return fs.readFileSync(path.resolve(filePath), "utf8");
 }
 
 export function write(filePath = "", data = "") {
-  fs.writeFileSync(filePath, data, "utf8");
+  fs.writeFileSync(path.resolve(filePath), data, "utf8");
   return true;
 }
 
 export function exists(filePath = "") {
-  return fs.existsSync(filePath);
+  return fs.existsSync(path.resolve(filePath));
+}
+
+export function mkdir(dirPath = "") {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+  }
+  return true;
 }
