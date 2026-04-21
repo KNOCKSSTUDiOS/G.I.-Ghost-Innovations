4322229e9c306e00fe1a13d78843cd79df76c1c4
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-21 10:35:50 -0700

    Update logger.js

--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -1,37 +1,11 @@
-const LEVELS = {
-  debug: 0,
-  info: 1,
-  warn: 2,
-  error: 3
-};
-
-let currentLevel = LEVELS.info;
-
-export function setLogLevel(level = "info") {
-  if (LEVELS[level] !== undefined) {
-    currentLevel = LEVELS[level];
-  }
-}
-
-export function log(level, ...args) {
-  if (LEVELS[level] >= currentLevel) {
-    console[level](...args);
-  }
-}
-
-export function debug(...args) {
-  log("debug", ...args);
-}
-
 export function info(...args) {
-  log("info", ...args);
+  console.log("[INFO]", ...args);
 }
 
 export function warn(...args) {
-  log("warn", ...args);
+  console.warn("[WARN]", ...args);
 }
 
 export function error(...args) {
-  log("error", ...args);
+  console.error("[ERROR]", ...args);
 }
-
