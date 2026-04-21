453455f0bf4ce1a59e39b5ce9aca26373c44b078
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-21 12:13:10 -0700

    Create sanitize.js

---
+++ backend/src/utils/sanitize.js
@@ -0,0 +1,9 @@
+export function sanitize(str = "") {
+  if (typeof str !== "string") return "";
+  return str
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
+
