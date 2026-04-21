dc16b7ff0f96022a807ba434fabeb5b8e9f83840
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-21 11:34:13 -0700

    Update math.js

--- backend/src/utils/math.js
+++ backend/src/utils/math.js
@@ -6,7 +6,7 @@ export function lerp(a = 0, b = 1, t = 0.5) {
   return a + (b - a) * t;
 }
 
-export function roundTo(num = 0, decimals = 0) {
+export function round(num = 0, decimals = 0) {
   const factor = Math.pow(10, decimals);
   return Math.round(num * factor) / factor;
 }
